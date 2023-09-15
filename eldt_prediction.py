35059d075f5606181791ccdba0c903bb167884a3
Pedro Teles <pedroteles1313@gmail.com>
2023-09-14 21:19:23 -0300

    final_df is almost ready, missing only cat-62 data

--- eldt_prediction.py
+++ eldt_prediction.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import dotenv
 import os
-from utils import FetchData, MetarExtender
+from utils import FetchData, MetarExtender, MergeDataSets
 
 dotenv.load_dotenv()
 
@@ -18,25 +18,43 @@ endpoints = [endpoint for endpoint in fetcher.ENDPOINTS if endpoint != "cat-62"]
 
 # Uncomment to fetch data from API
 ##endpoints_data = {endpoint: fetcher.fetch_endpoint(endpoint, start_date, end_date) for endpoint in endpoints}
-
 endpoints_data = {endpoint: pd.read_parquet(f"data/{endpoint}.parquet") for endpoint in endpoints}
 
-# %%
-endpoints_data["metaf"]["metaf"] = endpoints_data["metaf"]["metaf"].apply(lambda x: x.strip("\n").strip("="))
+# 'Troca de Cabeceira' (TC) is a runway change. For some reason, the API returns the runway code without the 'SB' prefix.
+endpoints_data["tc-prev"]["aero"] = "SB" + endpoints_data["tc-prev"]["aero"]
 
-parsed_metar = []
-for index, row in endpoints_data["metaf"].iterrows():
-    metar_str, modification_dict = MetarExtender.clean_metar_string(row["metaf"])
+endpoints_data["tc-real"]["aero"] = "SB" + endpoints_data["tc-real"]["aero"]
 
-    try:
-        obs = MetarExtender(metar_str)
-        metar_dict = obs.get_metar_dict()
-    except:
-        print(f"Error parsing METAR: {index, metar_str}")
+# TC-real has some useless columns
+endpoints_data["tc-real"] = endpoints_data["tc-real"].drop(["nova_cabeceira", "antiga_cabeceira"], axis=1)
+endpoints_data["tc-real"]["troca_real"] = 1
+
+# The satellite data is returned in a different format than the other endpoints.
+endpoints_data["satelite"] = endpoints_data["satelite"]\
+    .rename(columns={"data": "hora"})\
+    .drop("tamanho", axis=1)
+
+endpoints_data["satelite"]["hora"] = pd.to_datetime(endpoints_data["satelite"]["hora"])
+
+#%%
+for key, value in endpoints_data.items():
+    if key == "satelite":
         continue
 
-    metar_dict["hora"] = row["hora"]
-    metar_dict["aero"] = row["aero"]
-    metar_dict.update(modification_dict)
+    timestamp_column = "dt_dep" if key == "bimtra" else "hora"
 
-    parsed_metar.append(metar_dict)
+    endpoints_data[key][timestamp_column] = pd.to_datetime(endpoints_data[key][timestamp_column], unit='ms')
+
+#%%
+merger = MergeDataSets(endpoints_data["bimtra"])\
+    .merge_with_espera(endpoints_data["esperas"])\
+    .merge_with_metaf(endpoints_data["metaf"])\
+    .merge_with_metar(endpoints_data["metar"])\
+    .merge_with_tc_prev(endpoints_data["tc-prev"])\
+    .merge_with_tc_real(endpoints_data["tc-real"])\
+    .merge_with_satelite(endpoints_data["satelite"])
+
+final_df = merger.bimtra_df
+
+final_df["troca_real"] = final_df["troca_real"].fillna(0)
+# %%
--- utils.py
+++ utils.py
@@ -5,8 +5,100 @@ import re
 import time
 from metar import Metar
 
+class MergeDataSets:
+    AIRPORT_COLUMN = "aero"
+    HOUR_COLUMN = "hora"
+
+    def __init__(self, bimtra_df: pd.DataFrame):
+        self.bimtra_df = bimtra_df
+
+    def _merge_dataframes(self, left_on_cols, right_df, suffixes):
+        # Merge two dataframes and return the result
+        return self.bimtra_df.merge(
+            right_df, left_on=left_on_cols, right_on=[self.AIRPORT_COLUMN, self.HOUR_COLUMN], how="left", suffixes=suffixes
+        )
+
+    def _floor_departure_date(self):
+        self.bimtra_df["dt_dep_floor"] = self.bimtra_df["dt_dep"].dt.floor('H')
+
+    def _ceil_departure_date(self):
+        self.bimtra_df["dt_dep_ceiling"] = self.bimtra_df["dt_dep"].dt.ceil('H')
+
+    def merge_with_espera(self, wait_df: pd.DataFrame) -> 'MergeDataSets':
+        # To merge, we must floor the departure date to the hour and lag it by one hour
+        self._floor_departure_date()
+        self.bimtra_df["dt_dep_floor"] = self.bimtra_df["dt_dep_floor"] - pd.to_timedelta(1, unit='h')
+
+        self.bimtra_df = self._merge_dataframes(
+            left_on_cols=["destino", "dt_dep_floor"],
+            right_df=wait_df,
+            suffixes=("", "_esperas")
+        ).drop("dt_dep_floor", axis=1)
+        
+        return self
+
+    def merge_with_metaf(self, metaf_df: pd.DataFrame) -> 'MergeDataSets':
+        self._ceil_departure_date()
+
+        self.bimtra_df = self._merge_dataframes(
+            left_on_cols=["destino", "dt_dep_ceiling"],
+            right_df=metaf_df,
+            suffixes=("", "_metaf")
+        ).drop("dt_dep_ceiling", axis=1)
+        
+        return self
+
+    def merge_with_metar(self, metar_df: pd.DataFrame) -> 'MergeDataSets':
+        self._floor_departure_date()
+
+        self.bimtra_df = self._merge_dataframes(
+            left_on_cols=["destino", "dt_dep_floor"],
+            right_df=metar_df,
+            suffixes=("", "_metar")
+        ).drop("dt_dep_floor", axis=1)
+        
+        return self
+
+    def merge_with_tc_prev(self, tc_prev: pd.DataFrame) -> 'MergeDataSets':
+        self._ceil_departure_date()
+
+        self.bimtra_df = self._merge_dataframes(
+            left_on_cols=["destino", "dt_dep_ceiling"],
+            right_df=tc_prev,
+            suffixes=("", "_tcp")
+        ).drop("dt_dep_ceiling", axis=1)
+        
+        return self
+
+    def merge_with_tc_real(self, tc_real: pd.DataFrame) -> 'MergeDataSets':
+        self._floor_departure_date()
+
+        tc_real["hora"] = tc_real["hora"].dt.floor('H')
+
+        self.bimtra_df = self._merge_dataframes(
+            left_on_cols=["destino", "dt_dep_floor"],
+            right_df=tc_real,
+            suffixes=("", "_tcr")
+        ).drop("dt_dep_floor", axis=1)
+        
+        return self
+
+    def merge_with_satelite(self, satelite: pd.DataFrame) -> 'MergeDataSets':
+        self._floor_departure_date()
+
+        self.bimtra_df = self.bimtra_df.merge(
+            satelite, left_on="dt_dep_floor", right_on="hora", how="left", suffixes=("", "_sat")
+        ).drop("dt_dep_floor", axis=1)
+        
+        return self
+
 class FetchData:
     ENDPOINTS = ["bimtra", "cat-62", "esperas", "metaf", "metar", "satelite", "tc-prev", "tc-real"]
+    BASE_URL = "http://montreal.icea.decea.mil.br:5002/api/v1/"
+    VALID_DATE_FORMATS = {
+        "cat-62": r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}$",
+        "other": r"^\d{4}-\d{2}-\d{2}$",
+    }
 
     def __init__(self, api_token):
         self.api_token = api_token
@@ -20,14 +112,30 @@ class FetchData:
         if endpoint not in self.ENDPOINTS:
             raise ValueError(f"Endpoint not recognized. Must be one of {', '.join(self.ENDPOINTS)}")
 
-        date_format = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}$' if endpoint == "cat-62" else r'^\d{4}-\d{2}-\d{2}$'
+        date_format = self.VALID_DATE_FORMATS["cat-62"] if endpoint == "cat-62" else self.VALID_DATE_FORMATS["other"]
 
         if not self._is_valid_date_format(date_format, start_date) or not self._is_valid_date_format(date_format, end_date):
             raise ValueError(f"Start and end dates must be in the right format ({date_format}).")
 
-        url = f"http://montreal.icea.decea.mil.br:5002/api/v1/{endpoint}?token={self.api_token}&idate={start_date}&fdate={end_date}"
+        url = f"{self.BASE_URL}{endpoint}?token={self.api_token}&idate={start_date}&fdate={end_date}"
         response = requests.get(url)
+        response.raise_for_status()
         return pd.DataFrame(response.json())
+
+    def fetch_cat_62(self, start_date: str, end_date: str) -> pd.DataFrame:
+        date_range = pd.date_range(start=start_date, end=end_date, freq=f'2D')
+
+        dfs = []
+        for date in date_range:
+            start_date = date.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            end_date = (date + pd.Timedelta(days=2)).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            try:
+                dfs.append(self.fetch_endpoint("cat-62", start_date, end_date))
+            except Exception as e:
+                print(f"Error fetching data for {date}: {e}")
+
+        return pd.concat(dfs, ignore_index=True)
+        
 
 class MetarExtender(Metar.Metar):
     def __init__(self, metar_str):
